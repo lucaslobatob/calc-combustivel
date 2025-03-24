8233f945536ae8a5926c6353ef6907885ecc8383
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2025-03-24 11:31:29 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import './App.css'
+import logoImg from './assets/logo.png'
+
+function App() {
+  return (
+    <div>
+      <main className="container">
+        <img
+          className='logo'
+          src={logoImg}
+          alt="Logo da Calculadora Combustível"
+        />
+        <h1 className="title">Qual melhor opção?</h1>
+
+        <form className="form">
+          <label>Álcool (preço por litro):</label>
+          <input
+            className="input"
+            type='number'
+            placeholder='4,90'
+            min="1"
+            step="0.01"
+            required
+          />
+
+          <label>Gasolina (preço por litro):</label>
+          <input
+            className="input"
+            type='number'
+            placeholder='4,90'
+            min="1"
+            step="0.01"
+            required
+          />
+
+          <input
+            className="button"
+            type="submit"
+            value="Calcular"
+          />
+        </form>
+      </main>
+    </div>
+  )
+}
+
+export default App
